9b31a0ca33e35ac017418e7941d3b4044e3ad88f
Fiza Hussain <fiza16hussain@gmail.com>
2023-06-06 21:38:36 +0530

    added section 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from "./Components/Header.js"
+import Section1 from "./Components/Section1.js"
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Section1 />
     </div>
   );
 }
---
+++ src/Components/Section1.js
@@ -0,0 +1,45 @@
+import "./Section1.css";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+
+export default function Section1(params) {
+  return (
+    <div className="section1-main">
+      <div>
+        <p>
+          Along Dusty Roads brings you the beauty of a life of travel,
+          personally curated by us, Andrew & Emily.
+          <br />
+          <br />
+          Our crafted guides, thoughtful narratives, and original photography
+          journals wed wanderlust and practical advice to help and inspire
+          curious travellers like you to make the most of your own slow,
+          sustainable adventures.
+        </p>
+        <h3>WE WANT TO HELP YOU TO TRAVEL MORE, TRAVEL BETTER.</h3>
+        <div className="social-links">
+        <a className="mui" href="@">
+              <InstagramIcon />
+            </a>
+            <a className="mui" href="@">
+              <FacebookIcon />
+            </a>
+            <a className="mui" href="@">
+              <TwitterIcon />
+            </a>
+            <a className="mui" href="@">
+              <YouTubeIcon />
+            </a>
+
+        </div>
+        
+        
+      </div>
+      <div>
+        <img className="section1-img" src="https://images.squarespace-cdn.com/content/v1/53172b1be4b0cc8a5401db4a/1564529837147-XEF10BWTY1J0UOKLPLBH/DSC02497.jpg?format=500w" alt="girl" />
+      </div>
+    </div>
+  );
+}
